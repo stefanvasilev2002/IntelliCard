4f478dfc3f36f4c7d9c7dacc1ea788331c80571c
omniscient <vasilevstefan2002@gmail.com>
2025-06-03 12:41:20 +0200

    add cors for deployed frontend

--- src/main/java/com/finki/intellicard/config/SecurityConfig.java
+++ src/main/java/com/finki/intellicard/config/SecurityConfig.java
@@ -72,7 +72,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://intellicard-frontend.onrender.com"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
